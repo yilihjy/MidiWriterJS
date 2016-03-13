52b1466fa2b030e21e25a397bb9b55a8ad30baad
Garrett Grimm <ggrimm@prudentpublishing.com>
2016-03-13 01:28:18 -0800

    learning

--- midi-writer-js.js
+++ midi-writer-js.js
@@ -8,7 +8,21 @@ MIDI.constants = {
 	HEADER_CHUNK_LENGTH  	: "\x00\x00\x00\x06", // Header size for SMF
 	HEADER_CHUNK_FORMAT0    : ["\x00", "\x00"], // Midi Type 0 id
 	HEADER_CHUNK_FORMAT1    : ["\x00", "\x01"], // Midi Type 1 id
-	HEADER_CHUNK_DIVISION   : ["\x00", "\x80"] // Defaults to 128 ticks per beat
+	HEADER_CHUNK_DIVISION   : ["\x00", "\x80"], // Defaults to 128 ticks per beat
+	TRACK_CHUNK_TYPE		: "MTrk",
+	META_EVENT_ID			: "\xFF",
+	META_TEXT_ID			: "\x01",
+	META_COPYRIGHT_ID		: "\x02",
+	META_TRACK_NAME_ID		: "\x03",
+	META_INSTRUMENT_NAME_ID : "\x04",
+	META_LYRIC_ID			: "\x05",
+	META_MARKER_ID			: "\x06",
+	META_CUE_POINT			: "\x07",
+	META_END_OF_TRACK_ID	: "\x2F",
+	META_TEMPO_ID			: "\x51",
+	META_SMTPE_OFFSET		: "\x54",
+	META_TIME_SIGNATURE_ID	: "\x58",
+	META_KEY_SIGNATURE_ID	: "\x59"
 };
 
 MIDI.Chunk = function(fields) {
@@ -19,22 +33,58 @@ MIDI.Chunk = function(fields) {
 	return this;
 };
 
-MIDI.NoteEvent = function() {
-
+MIDI.NoteOnEvent = function() {
+	// note, octave, duration
+	return ["\x90", "\x3C"/*note*/, "\x40"];
 };
 
-MIDI.MetaEvent = function() {
+MIDI.NoteOffEvent = function() {
+	return ["\x80", "\x3C", "\x40"];
+};
 
+MIDI.MetaEvent = function(fields) {
+	this.data = [MIDI.constants.META_EVENT_ID];
 };
 
 MIDI.SysexEvent = function() {
 
 };
 
-MIDI.Writer = function() {
-	console.log(bto());
+MIDI.Writer = function(events) {
+	this.data = [];
+
+	// Header chunk
+	this.data.push(new MIDI.Chunk({
+							type: MIDI.constants.HEADER_CHUNK_TYPE, 
+							data: [].concat(MIDI.constants.HEADER_CHUNK_FORMAT0, ["\x00", "\x01"], MIDI.constants.HEADER_CHUNK_DIVISION)}));
+
+	// Track chunks
+	var track = new MIDI.Chunk({
+		type: MIDI.constants.TRACK_CHUNK_TYPE,
+		data: ["\x90", "\x3C"/*note*/, "\x40"]
+	});
+
+	this.data.push(track);
+
+
+	console.log('data:audio/midi;base64,' + btoa(this.buildFile(this.data)));
 };
 
+MIDI.Writer.prototype.buildFile = function(data) {
+	var build = "";
+
+	// Data consists of chunks which consists of data
+	for (var i in data) {
+		build += data[i].type + data[i].size + data[i].data.join('');
+	}
+
+	return build;
+};
+
+/**
+ * Utilities
+ *
+ */
 MIDI.numberToBytes = function(number, neededBytes) {
 	var str = number.toString(16);
 
@@ -61,13 +111,7 @@ MIDI.joinHexArray = function(array) {
 	return hexString;
 }
 
-var header = new MIDI.Chunk({
-							type: MIDI.constants.HEADER_CHUNK_TYPE, 
-							data: [].concat(MIDI.constants.HEADER_CHUNK_FORMAT0, ["\x00", "\x01"], MIDI.constants.HEADER_CHUNK_DIVISION)});
-console.log(btoa(header.type + header.size + header.data.join('')));
-
-
-
+new MIDI.Writer();
 
 
 
