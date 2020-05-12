513079bb7610ec2da7a34c3e13f819a27d2900d6
Garrett Grimm <garrett@grimmdude.com>
2020-05-11 19:47:08 -0700

    Export NoteOn and NoteOff event classes.  Add a little shameless love for library.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import {Constants} from './constants.js';
+import {NoteOnEvent} from './note-events/note-on-event.js';
+import {NoteOffEvent} from './note-events/note-off-event.js';
 import {NoteEvent} from './note-events/note-event.js';
 import {PitchBendEvent} from './meta-events/pitch-bend-event.js';
 import {ProgramChangeEvent} from './meta-events/program-change-event.js';
@@ -9,6 +11,8 @@ import {Writer} from './writer.js';
 
 export default {
   Constants,
+  NoteOnEvent,
+  NoteOffEvent,
   NoteEvent,
   PitchBendEvent,
   ProgramChangeEvent,
